cadaf1b3329cbee68be4fd212da3bcdc0727b80a
beparypartho <pallobbiswas326@gmail.com>
2023-06-17 21:47:39 +0600

    done

--- src/pages/leader-board/[id].vue
+++ src/pages/leader-board/[id].vue
@@ -9,8 +9,8 @@
       <div v-else class="w-full md:w-[60%] px-[20px]">
         <!-- first 3 -->
         <div v-if="rankings && rankings.length">
-          <div class="flex justify-center items-end gap-x-2 h-[190px] w-full">
-            <div class="overflow-hidden h-[170px] bg-white w-full rounded-lg shadow-md flex flex-col items-center">
+          <div class="flex justify-center items-end gap-x-2 h-[210px] w-full">
+            <div class="overflow-hidden h-[190px] bg-white w-full rounded-lg shadow-md flex flex-col items-center">
               <img class="h-16 w-16 mt-3" src="/images/rank/2.gif" alt="">
               <div class="py-2 text-center">
                 <p class="text-[12px]">{{ rankings[0]?.user.name }}</p>
@@ -18,7 +18,7 @@
                 <p class="text-[12px]">Score : {{ rankings[0]?.score }}</p>
               </div>
             </div>
-            <div class="overflow-hidden h-[190px] bg-white w-full rounded-lg shadow-md flex flex-col items-center">
+            <div class="overflow-hidden h-[210px] bg-white w-full rounded-lg shadow-md flex flex-col items-center">
               <img class="h-16 w-16 mt-3" src="/images/rank/1.gif" alt="">
               <div class="py-2 text-center">
                 <p class="text-[12px]">{{ rankings[1]?.user.name }}</p>
@@ -26,7 +26,7 @@
                 <p class="text-[12px]">Score : {{ rankings[1]?.score }}</p>
               </div>
             </div>
-            <div class="overflow-hidden h-[170px] bg-white w-full rounded-lg shadow-md flex flex-col items-center">
+            <div class="overflow-hidden h-[190px] bg-white w-full rounded-lg shadow-md flex flex-col items-center">
               <img class="h-16 w-16 mt-3" src="/images/rank/3.gif" alt="">
               <div class="py-2 text-center">
                 <p class="text-[12px]">{{ rankings[2]?.user.name }}</p>
@@ -37,7 +37,7 @@
           </div>
           <!-- all rankin -->
           <div v-if="rankings && rankings.length >= 3" class="bg-[#F6F8FC] rounded-md">
-            <div v-for="(rank, i) in rankings.slice(3)" :key="i">
+            <div class="rank-card" v-for="(rank, i) in rankings.slice(3)" :key="i">
               <div
                 :class="rank?.user?.user_id == userScore?.user?._id ? 'border-2 border-blue-500 bg-[#E9F8FF] py-2' : 'bg-white py-2'"
                 class="rounded-md my-2 justify-between flex items-center px-2">
@@ -62,17 +62,21 @@
           </div>
           <div class="flex justify-center">
             <div v-if="userScore && Object.keys(userScore).length" class="fixed bottom-1 w-[86%]">
-              <div class="bg-[#E4EFFF] w-[100%] rounded-md px-3 py-2">
-                <div class="flex justify-between items-center">
-                  <div>
-                    <p class="text-[16px]">{{ userScore?.user.name }}</p>
-                    <p class="text-[14px]">{{ userScore?.user.institute }}</p>
+              <div class="bg-[#b6cac8] py-3 rounded-md my-2 justify-between flex items-center px-2">
+                <div class="flex w-[100%] items-center">
+                  <div
+                    class="w-10 mr-4 h-10 bg-[#E4EFFF] text-[#045C96] text-[14px] flex justify-center items-center rounded-lg">
+                    {{ userScore?.rank }}
                   </div>
-                  <div>
-                    <div class="text-[14px] flex flex-col items-center">
-                      <p>Score</p>
-                      <p>{{ userScore?.score }}</p>
-                    </div>
+                  <div class="w-[50%] text-left">
+                    <p class="text-[14px] leading-none">{{ userScore?.user?.name }}</p>
+                    <p class="text-[12px] first-line:leading-none pt-2">{{ userScore?.user?.institute }}</p>
+                  </div>
+                </div>
+                <div class=" my-[-5px] flex-end">
+                  <div class="">
+                    <p class="text-[14px]">Score</p>
+                    <p class="text-center text-[14px]"> {{ userScore?.score }} </p>
                   </div>
                 </div>
               </div>
@@ -90,69 +94,6 @@
 </template>
 
 <script setup>
-
-const fakeRank = [
-  {
-    name: "Dummy student 1",
-    roll: "A23eerAS",
-    institution: "Nextive solution",
-    duration: "12:20",
-    total_mark: "120",
-    marks: "50",
-  },
-  {
-    name: "Dummy student 2",
-    roll: "A23eerAS",
-    institution: "Nextive solution",
-    duration: "12:20",
-    total_mark: "120",
-    marks: "50",
-  },
-  {
-    name: "Dummy student 3",
-    roll: "A23eerAS",
-    institution: "Nextive solution",
-    duration: "12:20",
-    total_mark: "120",
-    marks: "50",
-  },
-  {
-    name: "Dummy student 4",
-    roll: "A23eerAS",
-    institution: "Nextive solution",
-    duration: "12:20",
-    total_mark: "120",
-    marks: "50",
-    user: {
-      user_id: 1
-    }
-  },
-  {
-    name: "Dummy student 5",
-    roll: "A23eerAS",
-    institution: "Nextive solution",
-    duration: "12:20",
-    total_mark: "120",
-    marks: "50",
-  },
-  {
-    name: "Dummy student 6",
-    roll: "A23eerAS",
-    institution: "Nextive solution",
-    duration: "12:20",
-    total_mark: "120",
-    marks: "50",
-  },
-  {
-    name: "Dummy student 7",
-    roll: "A23eerAS",
-    institution: "Nextive solution",
-    duration: "12:20",
-    total_mark: "120",
-    marks: "50",
-  },
-];
-
 
 onMounted(() => {
   setTimeout(() => {
@@ -187,4 +128,8 @@ const init = async () => {
 
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.rank-card:last-child{
+  margin-bottom: 90px;
+}
+</style>
